4fec17880119ac49bbac6e036ca77bbb9e8e43f2
Kai <thiskaiguy@gmail.com>
2019-02-28 16:51:10 +0000

    Create an empty attribute macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn windows_dll(metadata: TokenStream, input: TokenStream) -> TokenStream {
+    unimplemented!("{:?}", metadata)
 }
