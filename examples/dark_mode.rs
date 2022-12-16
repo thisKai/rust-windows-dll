e1d065b5923d70ce906380c26e6c9073a970ae95
Kai <thiskaiguy@gmail.com>
2022-12-16 17:17:10 +0000

    Fix dark mode example for windows-sys

--- examples/dark_mode.rs
+++ examples/dark_mode.rs
@@ -236,3 +236,113 @@ mod platform {
         }
     }
 }
+
+#[cfg(feature = "windows-sys")]
+mod platform {
+    use super::*;
+    use windows_sys::Win32::Foundation::{BOOL, HWND, NTSTATUS};
+
+    type DWORD = u32;
+    type ULONG = u32;
+    type WCHAR = u16;
+    type PVOID = *mut c_void;
+    #[allow(non_camel_case_types)]
+    type SIZE_T = usize;
+
+    const FALSE: BOOL = 0;
+
+    #[allow(non_snake_case)]
+    #[inline]
+    fn NT_SUCCESS(status: NTSTATUS) -> bool {
+        status >= 0
+    }
+
+    static WIN10_BUILD: Lazy<Option<DWORD>> = Lazy::new(|| {
+        #[dll(ntdll)]
+        extern "system" {
+            #[allow(non_snake_case)]
+            fn RtlGetVersion(lpVersionInformation: *mut OSVERSIONINFOW) -> NTSTATUS;
+        }
+
+        #[allow(non_snake_case)]
+        #[repr(C)]
+        struct OSVERSIONINFOW {
+            dwOSVersionInfoSize: ULONG,
+            dwMajorVersion: ULONG,
+            dwMinorVersion: ULONG,
+            dwBuildNumber: ULONG,
+            dwPlatformId: ULONG,
+            szCSDVersion: [WCHAR; 128],
+        }
+        unsafe {
+            if !RtlGetVersion::exists() {
+                return None;
+            }
+            let mut version_info = OSVERSIONINFOW {
+                dwOSVersionInfoSize: 0,
+                dwMajorVersion: 0,
+                dwMinorVersion: 0,
+                dwBuildNumber: 0,
+                dwPlatformId: 0,
+                szCSDVersion: [0; 128],
+            };
+            let status = RtlGetVersion(&mut version_info);
+
+            if NT_SUCCESS(status)
+                && version_info.dwMajorVersion == 10
+                && version_info.dwMinorVersion == 0
+            {
+                Some(version_info.dwBuildNumber)
+            } else {
+                None
+            }
+        }
+    });
+
+    static DARK_MODE_SUPPORTED: Lazy<bool> = Lazy::new(|| match *WIN10_BUILD {
+        Some(build) => build >= 17763,
+        None => false,
+    });
+
+    pub fn dark_dwm_decorations(minifb_hwnd: *mut c_void, enable_dark_mode: bool) -> bool {
+        let hwnd = minifb_hwnd as HWND;
+
+        #[allow(non_snake_case)]
+        type WINDOWCOMPOSITIONATTRIB = u32;
+        const WCA_USEDARKMODECOLORS: WINDOWCOMPOSITIONATTRIB = 26;
+
+        #[allow(non_snake_case)]
+        #[repr(C)]
+        struct WINDOWCOMPOSITIONATTRIBDATA {
+            Attrib: WINDOWCOMPOSITIONATTRIB,
+            pvData: PVOID,
+            cbData: SIZE_T,
+        }
+
+        #[dll(user32)]
+        extern "system" {
+            #[allow(non_snake_case)]
+            fn SetWindowCompositionAttribute(
+                h_wnd: HWND,
+                data: *mut WINDOWCOMPOSITIONATTRIBDATA,
+            ) -> BOOL;
+        }
+
+        if *DARK_MODE_SUPPORTED && unsafe { SetWindowCompositionAttribute::exists() } {
+            unsafe {
+                let mut is_dark_mode_bigbool = BOOL::from(enable_dark_mode);
+                let mut data = WINDOWCOMPOSITIONATTRIBDATA {
+                    Attrib: WCA_USEDARKMODECOLORS,
+                    pvData: &mut is_dark_mode_bigbool as *mut _ as _,
+                    cbData: mem::size_of::<BOOL>(),
+                };
+
+                let status = SetWindowCompositionAttribute(hwnd, &mut data);
+
+                status != FALSE
+            }
+        } else {
+            false
+        }
+    }
+}
